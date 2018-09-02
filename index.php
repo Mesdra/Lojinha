7e062e0d029b0756e3006a2cbc9e73863de971aa
Mesdra <vinicavala97@gmail.com>
2018-09-02 15:36:26 -0300

    menu inicial pronto

--- index.php
+++ index.php
@@ -8,10 +8,56 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        	<title>Menu Horizontal</title>
+	<style type="text/css">
+	<!--
+		body {
+			padding:0px;
+			margin:0px;
+		}
+ 
+		#menu ul {
+			padding:0px;
+			margin:0px;
+			float: left;
+			width: 100%;
+			background-color:#EDEDED;
+			list-style:none;
+			font:80% Tahoma;
+		}
+ 
+		#menu ul li { display: inline; }
+ 
+		#menu ul li a {
+			background-color:#EDEDED;
+			color: #333;
+			text-decoration: none;
+			border-bottom:3px solid #EDEDED;
+			padding: 2px 10px;
+			float:left;
+		}
+ 
+		#menu ul li a:hover {
+			background-color:#D6D6D6;
+			color: #6D6D6D;
+			border-bottom:3px solid #EA0000;
+		}
+	-->
+	</style>
+        
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+     	<div id="menu">
+		<ul>
+			<li><a href="">Home</a></li>
+			<li><a href="">Eestou com Sorte</a></li>
+			<li><a href="">Roupas Esportivas</a></li>
+			<li><a href="">Roupas Sociais</a></li>
+			<li><a href="">Roupas Infantis</a></li>
+			<li><a href="">Login</a></li>
+                        <li><a href="/Lojinha/cadastro/indexCadastro.php">Novo Usuario</a></li>
+		</ul>
+	</div>
+        
     </body>
 </html>
